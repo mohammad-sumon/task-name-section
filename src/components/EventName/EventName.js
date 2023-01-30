56a4bcc7c5bf72ccaeb54a3729225765119d2909
Mohammad Sumon <sumon3011@gmail.com>
2023-01-30 17:17:09 +0600

    use react and simple css

---
+++ src/components/EventName/EventName.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import './EventName.css';
+
+const EventName = () => {
+    return (
+        <div className='eventDiv'>
+            <h2 className='eventHeading'>RECENT CONTESTS</h2>
+            <div className='eventGrid'>
+            <div>
+                <h3>First Event</h3>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptas laborum sed aliquam deserunt! Quos perferendis aliquid delectus rem eveniet aut vero cum laborum minus. Dolore natus rerum voluptate debitis, quaerat porro voluptatem ab velit quidem. Maxime iusto fuga alias sequi ducimus ratione, eveniet, atque sed, veniam tempora in dolor ex!</p>
+            </div>
+            <div>
+                <h3>Second Event</h3>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptas laborum sed aliquam deserunt! Quos perferendis aliquid delectus rem eveniet aut vero cum laborum minus. Dolore natus rerum voluptate debitis, quaerat porro voluptatem ab velit quidem. Maxime iusto fuga alias sequi ducimus ratione, eveniet, atque sed, veniam tempora in dolor ex!</p>
+            </div>
+            <div>
+                <h3>Third Event</h3>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptas laborum sed aliquam deserunt! Quos perferendis aliquid delectus rem eveniet aut vero cum laborum minus. Dolore natus rerum voluptate debitis, quaerat porro voluptatem ab velit quidem. Maxime iusto fuga alias sequi ducimus ratione, eveniet, atque sed, veniam tempora in dolor ex!</p>
+            </div>
+            </div>        
+        </div>
+    );
+};
+
+export default EventName;
